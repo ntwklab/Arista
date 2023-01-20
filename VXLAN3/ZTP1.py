0ce4e72bdb4f6fe7e24ebe1a6f513a228fbea144
ntwklab <113145255+ntwklab@users.noreply.github.com>
2023-01-20 15:28:35 +0000

    Create ZTP1.py

---
+++ VXLAN3/ZTP1.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python
+
+from collections import Counter
+import subprocess
+import sys
+import time
+
+# Delay for a 60 seconds
+time.sleep(60)
+output = subprocess.Popen(["/usr/bin/FastCli", "-c", "show lldp neigh"], stdout=subprocess.PIPE).communicate()[0]
+out2 = list(iter(output.decode().split()))
+
+# Count interfaces for spine
+lookup = ["Ethernet1", "Ethernet2", "Ethernet3", "Ethernet4"]
+all_counts = Counter(out2)
+counts = {k: all_counts[k] for k in lookup}
+print(f"all counts {all_counts}")
+print(f"counts {counts}")
+print(f"counts {counts.values()}")
+
+# Determine if spine or leaf
+for value in counts.values():
+    print(value)
+    if value >= 4:
+        switch = "spine"
+        break
+    else:
+        switch = "leaf"
+
+# Determine spine number
+for key, value in counts.items():
+    if value >= 4:
+        spine_num = key[8:]
+        break
+
+# Assing spine number hostname and IP
+if switch == "spine":
+    hostname = "spine{}".format(spine_num)
+    ip_addr = "172.16.1.1{}".format(spine_num)
+
+# Determine and assign leaf hostname and IP
+if switch == "leaf":
+    out3 = list(iter(output.decode().splitlines()))
+    intnum = out3[8].split()[2][8:]
+    hostname = "leaf{}".format(intnum)
+    ip_addr = "172.16.1.1{}".format(intnum)
+
+
+# Add AEM for vxlan python script
+# download python script from tftp
+# create AEM
+
+
+# Create startup-config file
+f = open("/mnt/flash/startup-config", "w")
+f.write("\nhostname {}".format(hostname))
+f.write("\ninterface Management1\nip address {}/24".format(ip_addr))
+f.write("\nip routing")
+f.write("\nip route 0.0.0.0/0 172.16.1.1")
+# XMPP
+f.write("\nmanagement xmpp")
+f.write("\ndomain securitydemo.lab")
+f.write("\nusername {} password Stefan2020".format(hostname))
+f.write("\nsession privilege 15")
+f.write("\nserver 172.16.1.69")
+f.write("\nswitch-group vxlan")
+f.write("\nno shutdown")
+# Remove AEM ZTP1
+f.write("\nno event-handler ZTP1")
+# Add AEM for VXLAN
+f.write("\nevent-handler VXLAN_CONFIG")
+f.write("\ntrigger on-startup-config")
+# Reload switch to run VXLAN AEM
+f.write("\n action bash python3 /mnt/flash/AEM_VXLAN_DeviceReady.py; `FastCli -p 15 -c 'reload in 1 force reason VXLAN Configuration'`")
+
+f.close()
+sys.exit( 0 )
